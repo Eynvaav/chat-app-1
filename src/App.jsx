3637955b71eccbc272d41c2b119c80c9afc56a41
Eynvaav <142487085+Eynvaav@users.noreply.github.com>
2024-04-14 20:42:28 +0430

    set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PrivateRoutes from './components/PrivateRoutes';
 import Room from './pages/Room';
+import Login from './pages/Login';
 
 function App() {
 	return (
-		<>
-			<Room />
-		</>
+		<Router>
+			<Routes>
+				<Route
+					path='/login'
+					element={<Login />}
+				/>
+				<Route element={<PrivateRoutes />}>
+					<Route
+						path='/'
+						element={<Room />}
+					/>
+				</Route>
+			</Routes>
+		</Router>
 	);
 }
 
